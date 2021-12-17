cc083cdba8e8b621dde9a306c8fad0933d1cf67a
Alex Isogai <ax.isogai@hotmail.com>
2021-12-16 23:42:04 -0300

    Exercício método

---
+++ TesteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TesteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaA = new ContaCorrente();
+            ContaCorrente contaB = new ContaCorrente();
+
+            contaA.saldo += 500;
+            contaB.saldo += 1000;
+
+            Console.WriteLine("Saldo A: " + contaA.saldo);
+            Console.WriteLine("Saldo B: " + contaB.saldo);
+
+            contaA.Depositar(250);
+            bool resultadoSaqueA = contaA.Sacar(1000);
+
+            contaB.Transferir(1100, contaA);
+
+            Console.WriteLine("Saldo A: " + contaA.saldo);
+            Console.WriteLine(resultadoSaqueA);
+            Console.WriteLine("Saldo B: " + contaB.saldo);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
