1f31921b4a426a24fa184dec56392d2fba1a9554
Alex Isogai <ax.isogai@hotmail.com>
2022-02-21 20:17:10 -0300

    .

---
+++ 07-ByteBank/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    public class Cliente
+    {
+        private string _cpf;
+
+        public string Nome { get; set; }
+        public string CPF 
+        {
+            get 
+            { 
+                return _cpf;
+            }
+            set 
+            {   //Escrevo minha lógica de validaçãod e CPF
+                _cpf = value;
+            }
+        }
+        public string Profissao { get; set; }
+    }
+}
--- 07-ByteBank/Program.cs
+++ 07-ByteBank/Program.cs
@@ -6,7 +6,26 @@ namespace _07_ByteBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            ContaCorrente conta = new ContaCorrente(876, 86712540);
+            // ContaCorrente conta =new ContaCorrente();
+            //conta.Numero = 86712540;
+            // conta.Agencia = 867;
+            //conta.Agencia = -10;
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+            
+
+
+            ContaCorrente contaDaGabriela = new ContaCorrente(857, 86745820);
+
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            Console.ReadLine();
         }
     }
 }
